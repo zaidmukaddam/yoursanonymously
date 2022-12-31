2b136580d8762562599264ff73c039c7176a1af4
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2022-12-31 15:06:27 +0530

    feat: well eslint sucks

--- apps/web/src/components/Create.tsx
+++ apps/web/src/components/Create.tsx
@@ -18,26 +18,24 @@ export const Create = () => {
   };
 
   return (
-    <>
-      <section className='mx-auto max-w-screen-sm'>
-        <form onSubmit={handleSubmit} className='mb-2 flex space-x-2'>
-          <input
-            required
-            type='text'
-            value={username}
-            onChange={(e) => setUsername(e.target.value.toLowerCase())}
-            placeholder='Enter new username'
-            minLength={3}
-            maxLength={12}
-            className='settings-input'
-          />
-          <button type='submit' className='primary-btn flex-none'>
-            Set Username
-          </button>
-        </form>
+    <section className='mx-auto max-w-screen-sm'>
+      <form onSubmit={handleSubmit} className='mb-2 flex space-x-2'>
+        <input
+          required
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value.toLowerCase())}
+          placeholder='Enter new username'
+          minLength={3}
+          maxLength={12}
+          className='settings-input'
+        />
+        <button type='submit' className='primary-btn flex-none'>
+          Set Username
+        </button>
+      </form>
 
-        <Info message='You can still change your username later.' />
-      </section>
-    </>
+      <Info message='You can still change your username later.' />
+    </section>
   );
 };
